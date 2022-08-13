0356422afaf8f7d8e9d0faccf6d2f50d9da9d48e
Bailey Chittle <bachittle@gmail.com>
2022-08-12 20:07:34 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Typewriter from "./lib/Typewriter.svelte";
+</script>
+
+<h1><Typewriter text={'Bailey Chittle'} eraseText={false}/></h1>
+<p><Typewriter texts={[
+  "The Software and Web Developer that everyone should hire!",
+  "Motivated to Make a Better Tomorrow",
+  "while(isAlive) {eat();code();sleep();}",
+  "Star Web Designer",
+  "My CSS is !important",
+  `$("#Github").click();`,
+  `"The more you know, the more you realize you know nothing" -Socrates`,
+]}/></p>
---
+++ src/lib/Typewriter.svelte
@@ -0,0 +1,48 @@
+<script>
+  export let text = 'Bailey Chittle'; // single item to type
+  export let texts = []; // array of elements to type
+  export let eraseText = true;
+
+  let typedText = '';
+  const typeSpeed = 80;
+  
+  let textsIndex = 0;
+  if (texts.length != 0) {
+    text = texts[textsIndex]; // initialize text to first element, then change this on the eraseText reset.
+  }
+  
+  function typeWriter() {
+    if (typedText.length < text.length) {
+      typedText += text.charAt(typedText.length);
+      setTimeout(typeWriter, typeSpeed);
+    } else if (eraseText) {
+      setTimeout(() => {
+        typedText = '';
+        if (texts.length != 0) {
+          textsIndex++;
+          // reset textsIndex if at the end
+          if (textsIndex >= texts.length) {
+            textsIndex = 0;
+          }
+          text = texts[textsIndex];
+        }
+        setTimeout(typeWriter, 1000);
+      }, 2000);
+    }
+  }
+  setTimeout(typeWriter, typeSpeed);
+  
+  let animatedCaret = '';
+  export const caretChar = '|';
+  
+  function animateCaret() {
+    if (animatedCaret.length == 0) {
+      animatedCaret = '|';
+    } else { 
+      animatedCaret = '';
+    }
+  }
+  setInterval(animateCaret, 400);
+</script>
+
+{typedText}{animatedCaret}
