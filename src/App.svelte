697bb0602235ae6d4e7f6cac75b6654182ab7bdb
Bailey Chittle <bachittle@gmail.com>
2022-08-14 17:13:25 -0400

    fixed layout, added projects tab

--- src/App.svelte
+++ src/App.svelte
@@ -2,68 +2,110 @@
   import Typewriter from "./lib/Typewriter.svelte";
   import DiGithubBadge from 'svelte-icons/di/DiGithubBadge.svelte'
   import FaFileAlt from 'svelte-icons/fa/FaFileAlt.svelte'
-  import FaProjectDiagram from "svelte-icons/fa/FaProjectDiagram.svelte";
 import FaLinkedin from "svelte-icons/fa/FaLinkedin.svelte";
+import Projects from "./lib/Projects.svelte";
   
   const logos = {
     
   };
 </script>
 
-<div class="my-text">
-  <h1 class="title"><Typewriter text={'Bailey Chittle'} eraseText={false}/></h1>
-  <p class="description"><Typewriter texts={[
-    "Software engineer",
-    "Currently employed to write efficient C++ for ultrasonic systems",
-    `"The more you know, the more you realize you know nothing" -Socrates`,
-    "I have experience in web development and project management",
-    "while(isAlive) {eat();code();sleep();}",
-    "My CSS is !important",
-    `$("#Github").click();`,
-  ]}/></p>
-  
-</div>
+<div class="pages">
+  <div class="page intro">
+    <div class="my-text">
+      <h1 class="title"><Typewriter text={'Bailey Chittle'} eraseText={false}/></h1>
+      <p class="description"><Typewriter texts={[
+        "Software engineer",
+        "Currently employed to write efficient C++ for ultrasonic systems",
+        `"The more you know, the more you realize you know nothing" -Socrates`,
+        "I have experience in web development and project management",
+        "while(isAlive) {eat();code();sleep();}",
+        "My CSS is !important",
+        `$("#Github").click();`,
+      ]}/></p>
+      
+    </div>
 
-<div class="logo-icons">
-  <a class="icon gh-icon" href="#">
-    <DiGithubBadge />
-  </a>
-  <a class="icon resume-icon" href="#">
-    <FaFileAlt />
-  </a>
-  <a class="icon project-icon" href="#">
-    <FaProjectDiagram />
-  </a>
-  <a class="icon in-icon" href="#">
-    <FaLinkedin />
-  </a>
+    <div class="logo-icons">
+      <a class="icon gh-icon" href="https://github.com/bachittle">
+        <DiGithubBadge />
+      </a>
+      <a class="icon in-icon" href="https://linkedin.com/in/bailey-chittle">
+        <FaLinkedin />
+      </a>
+      <a class="icon resume-icon" href="#">
+        <FaFileAlt />
+      </a>
+    </div>
+  </div>
+  <div class="page">
+    <div class="projects">
+      <Projects />
+    </div>
+  </div>
 </div>
 
 <style>
-  .my-text {
-    text-align: center;
-    margin: 10% 0 2% 0;
+  /* pages resemble 2 cards. Side by side horizontally when wide, otherwise side by side vertically. */
+  .pages {
+    display: flex;
   }
-  .description {
-    height: 19px;
+  .page {
+    width: 50%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+  
+  @media (max-width: 820px) {
+    .pages {
+      display: flex;
+      flex-direction: column;
+    }
+    .page {
+      width: 100%;
+      height: 45vh;
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+    }
+  }
+  
+  /* intro displays name, description, and logos */
+  .intro {
+    text-align: center;
   }
   .title {
     height: 39px;
   }
+  .description {
+    height: 19px;
+    margin: 2em 0;
+  }
 
   .logo-icons {
-    margin: 0 25%;
+    margin: 0 10% 0 5%;
     display: flex;
     justify-content: space-around;
   }
   .icon {
     height: 100px;
-    margin: 0;
+    margin: 0 1em;
     color: white;
   }
   .gh-icon {
     position: relative;
-    bottom: 20px;
+    bottom: 15px;
+    left: 15px;
     height: 140px;
   }
+  
+  /* projects shows list of projects I worked on. Javascript list has data. Data is rendered via an each block */
+  .projects {
+    background-color: #1e1f2e;
+    padding: 2em 2em;
+    margin: 0 0;
+  }
+  
 </style>
---
+++ src/lib/Projects.svelte
@@ -0,0 +1,31 @@
+<h2>Projects</h2>
+<div class="cards">
+    <a href="https://github.com/bachittle/uwindsor-timetable-scraper" class="card">
+        <h3>UTable</h3>
+    </a>
+    <a href="https://github.com/bachittle/visual-sorting-online" class="card">
+        <h3>visual-sorting-online</h3>
+    </a>
+    <a href="https://github.com/bachittle/playlist-creator" class="card">
+        <h3>playlist creator</h3>
+    </a>
+</div>
+
+<style>
+    a {
+        color: white;
+    }
+    h2 {
+        text-align: center;
+    }
+    .cards {
+        display: flex;
+        justify-content: space-around;
+        flex-direction: column;
+        text-align: center;
+    }
+    
+    .card {
+        background-color: #0A161F;
+    }
+</style>
